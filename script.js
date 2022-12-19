59277c8f1a3714db634c7622ef0fe82d9e56ce2d
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2022-12-19 18:24:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -43,4 +43,14 @@ btn.addEventListener("click", function(e){
     console.log(data1_array);
     console.log(data2_array);
 
+    //Data atual
+
+    var data = new Date();
+    var dia = String(data.getDate()).padStart(2, '0');
+    var mes = String(data.getMonth() + 1).padStart(2, '0');
+    var ano = data.getFullYear();
+    dataAtual = ano + '-' + mes + '-' + dia;
+    console.log(dataAtual);
+
+    
 });
